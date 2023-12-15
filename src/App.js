5505fd683712c65698794291ccdb77a791b8f562
Mikhail Saburov <saburov1985@gmail.com>
2023-12-15 22:22:06 +1100

    refactor(App): стилизовал статьи

--- src/App.js
+++ src/App.js
@@ -22,7 +22,11 @@ function App() {
 
         <div className="text">
           <h2>A comprehensive list of 2023 tech layoffs</h2>
-          <p>
+          <p className="info">
+            <a className="autor">Mike Saburov</a>
+            <time>15.12.2023 22:13</time>
+          </p>
+          <p className="summary">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit
             eaque tempore, blanditiis totam provident sequi quod hic illo neque
             odio? Amet et voluptatibus quae ratione perspiciatis quia facilis
