82a6993aade18e942c3a503b7fe8607bb2b317d9
Mikhail Saburov <saburov1985@gmail.com>
2023-12-15 21:39:47 +1100

    refactor(App): набросал разметку постов

--- src/App.js
+++ src/App.js
@@ -12,6 +12,54 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="post">
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2023/11/oneplus-open.jpg"
+          alt=""
+        />
+        <div className="text">
+          <h2>A comprehensive list of 2023 tech layoffs</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit
+            eaque tempore, blanditiis totam provident sequi quod hic illo neque
+            odio? Amet et voluptatibus quae ratione perspiciatis quia facilis
+            dolores repudiandae?
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2023/11/CMC_7486.jpg"
+          alt=""
+        />
+        <div className="text">
+          <h2>Apple says it’s not planning a 27-inch iMac</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit
+            eaque tempore, blanditiis totam provident sequi quod hic illo neque
+            odio? Amet et voluptatibus quae ratione perspiciatis quia facilis
+            dolores repudiandae?
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2023/11/Xreal-Air-Beam.jpg"
+          alt=""
+        />
+        <div className="text">
+          <h2>
+            The iPhone 15’s best friend is an ultra-budget, stripped-down
+            version of Apple Vision Pro
+          </h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit
+            eaque tempore, blanditiis totam provident sequi quod hic illo neque
+            odio? Amet et voluptatibus quae ratione perspiciatis quia facilis
+            dolores repudiandae?
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
