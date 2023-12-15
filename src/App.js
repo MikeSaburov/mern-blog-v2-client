f54be16317db10f6ff9bfa6410a21a9b69d013c7
Mikhail Saburov <saburov1985@gmail.com>
2023-12-15 22:52:57 +1100

    fix(Layout): поправил путь к Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import './App.scss';
 
 import { Route, Routes } from 'react-router-dom';
-
-import { Header } from './Header';
+import { Layout } from './Layout';
 import { Post } from './Post';
 
 function App() {
   return (
     <Routes>
-      <Route
-        index
-        element={
-          <main>
-            <Header />
-            <Post />
-          </main>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Post />} />
 
-      <Route path="/login" element={<div>Login</div>} />
-      <Route path="/register" element={<div>Register Page</div>} />
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register Page</div>} />
+      </Route>
     </Routes>
   );
 }
