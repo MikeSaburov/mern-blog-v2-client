cc59f836cf71b8188ff0555d994cf9b7f9a2acd8
Mikhail Saburov <saburov1985@gmail.com>
2023-12-16 16:58:40 +1100

    feature(RegisterPage): создал функцию Register функция отправляет введеные данные на сервер

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const [userName, setUserName] = useState('');
+const [password, setPassword] = useState('');
+
+async function Register(e) {
+  e.preventDefault();
+  await fetch('http://localhost:400', {
+    method: 'POST',
+    body: JSON.stringify({ userName, password }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
 
 export const RegisterPage = () => {
   return (
-    <form className="register">
+    <form className="register" onSubmit={Register}>
       <h2>Регистрация</h2>
-      <input type="text" placeholder="Введите имя пользователя" />
-      <input type="password" placeholder="Введите пароль" />
+      <input
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        type="text"
+        placeholder="Введите имя пользователя"
+      />
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        placeholder="Введите пароль"
+      />
       <button>Зарегестрироваться</button>
     </form>
   );
