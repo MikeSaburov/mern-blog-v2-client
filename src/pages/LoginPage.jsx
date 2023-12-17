4c22989acac3959f347105ddcc1c0e8d65910086
Mikhail Saburov <saburov1985@gmail.com>
2023-12-17 13:59:57 +1100

    feature(LoginPage): создал функцию Login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const LoginPage = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function Login(e) {
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({ userName, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
   return (
-    <form className="login">
+    <form className="login" onSubmit={Login}>
       <h2>Войти</h2>
-      <input type="text" placeholder="Введите имя пользователя" />
-      <input type="password" placeholder="Введите пароль" />
+      <input
+        type="text"
+        placeholder="Введите имя пользователя"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Введите пароль"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Войти</button>
     </form>
   );
