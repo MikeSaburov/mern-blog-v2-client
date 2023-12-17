b64172ae5cf258dc4a67fd8cc87b9e4eddc2ec4c
Mikhail Saburov <saburov1985@gmail.com>
2023-12-17 20:58:57 +1100

    feature(UserContext): редактирование кода в контексте

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,7 +1,13 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
-  return <div>{children}</div>;
+  const [userInfo, setUserInfo] = useState({});
+
+  return (
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
