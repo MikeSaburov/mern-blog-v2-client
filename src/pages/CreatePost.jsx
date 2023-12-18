e2e0dc38689992d9ecc8935ae9522150a8715bff
Mikhail Saburov <saburov1985@gmail.com>
2023-12-18 21:39:52 +1100

    feature(CreatePost): реализовано добавление информации в запрос

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -29,9 +29,23 @@ export const CreatePost = () => {
   const [title, setTitle] = useState('');
   const [summary, setSummary] = useState('');
   const [content, setContent] = useState('');
+  const [files, setFiles] = useState('');
 
   function CreateNewPost(e) {
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('file', files[0]);
     e.preventDefault();
+    console.log(files);
+
+    fetch('http://localhost:4000/create', {
+      method: 'POST',
+      body: JSON.stringify({ title, summary, content }),
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    });
   }
 
   return (
@@ -48,7 +62,7 @@ export const CreatePost = () => {
         value={summary}
         onChange={(e) => setSummary(e.target.value)}
       />
-      <input type="file" />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
       <ReactQuill
         theme="snow"
         value={content}
