dcb0f6f22c13ea116d6ac0e5f4e0224a6d8db0b4
Mikhail Saburov <saburov1985@gmail.com>
2023-12-20 21:41:57 +1100

    feature(PostPage): набросал логику запроса на сервер
    запрос по id должен получать пост с сервера

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export const PostPage = () => {
+  const { id } = useParams();
+  const [postInfo, setPostInfo] = useState(null);
+  useEffect(() => {
+    console.log(id);
+    fetch(`http://localhost:4000/post/${id}`).then((res) => {
+      res.json().then((postInfo) => {
+        setPostInfo(postInfo);
+      });
+    });
+  }, []);
+
   return <div>PostPage</div>;
 };
