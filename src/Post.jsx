5f15a2d1dfb7fd40fd25ea124e2f73b6d26d3fe4
Mikhail Saburov <saburov1985@gmail.com>
2023-12-20 21:04:34 +1100

    refactor(Post): добавил id поста в ссылку

--- src/Post.jsx
+++ src/Post.jsx
@@ -3,17 +3,25 @@ import { format } from 'date-fns';
 import { Link } from 'react-router-dom';
 
 import { ru } from 'date-fns/locale';
-export const Post = ({ title, summary, cover, content, createdAt, author }) => {
+export const Post = ({
+  _id,
+  title,
+  summary,
+  cover,
+  content,
+  createdAt,
+  author,
+}) => {
   return (
     <div className="post">
       <div className="image">
-        <Link to={'/post/:id'}>
+        <Link to={`/post/${_id}`}>
           <img src={`http://localhost:4000/${cover}`} alt="" />
         </Link>
       </div>
 
       <div className="text">
-        <Link to={'/post/:id'}>
+        <Link to={`/post/${_id}`}>
           <h2>{title}</h2>
         </Link>
 
