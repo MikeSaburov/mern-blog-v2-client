f2597838097f34084ab4127f1c037f86c4ac1687
Mikhail Saburov <saburov1985@gmail.com>
2023-12-21 22:39:33 +1100

    feature(PostPage): добавил useContext(UserContext)
    начал реализацию редактирования статьи

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
 import { format } from 'date-fns';
 import { ru } from 'date-fns/locale';
+import { UserContext } from '../UserContext';
 
 export const PostPage = () => {
   const { id } = useParams();
   const [postInfo, setPostInfo] = useState(null);
+  const { userInfo } = useContext(UserContext);
+
   useEffect(() => {
-    console.log(id);
     fetch(`http://localhost:4000/post/${id}`).then((res) => {
       res.json().then((postInfo) => {
         setPostInfo(postInfo);
@@ -27,6 +29,14 @@ export const PostPage = () => {
       <h1>{postInfo.title}</h1>
       <h3>{postInfo.summary}</h3>
       <div className="author">Автор: {postInfo.author.userName}</div>
+
+      {userInfo.id === postInfo.author._id && (
+        <div className="edit-row">
+          <Link className="edit" to={'/edit'}>
+            Редактировать статью
+          </Link>
+        </div>
+      )}
       <div className="image">
         <img src={`http://localhost:4000/${postInfo.cover}`} alt="" />
       </div>
