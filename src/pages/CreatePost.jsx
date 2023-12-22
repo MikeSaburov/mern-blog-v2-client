908b15d65e1e230c5e05fa954d008c7fa055f80d
Mikhail Saburov <saburov1985@gmail.com>
2023-12-22 23:48:36 +1100

    refactor(CreatePost): убрал лишний код

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,31 +1,6 @@
 import React, { useState } from 'react';
-// import ReactQuill from 'react-quill';
-// import 'react-quill/dist/quill.snow.css';
 import { Navigate } from 'react-router-dom';
 import Editor from '../Editor';
-
-// const modules = {
-//   toolbar: [
-//     [{ header: [1, 2, false] }],
-//     ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-//     [{ list: 'ordered' }, { list: 'bullet' }],
-//     ['link', 'image'],
-//     ['clean'],
-//   ],
-// };
-
-// const formats = [
-//   'header',
-//   'bold',
-//   'italic',
-//   'underline',
-//   'strike',
-//   'blockquote',
-//   'list',
-//   'bullet',
-//   'link',
-//   'image',
-// ];
 
 export const CreatePost = () => {
   const [title, setTitle] = useState('');
@@ -73,14 +48,8 @@ export const CreatePost = () => {
         onChange={(e) => setSummary(e.target.value)}
       />
       <input type="file" onChange={(e) => setFiles(e.target.files)} />
-      <Editor />
-      {/* <ReactQuill
-        theme="snow"
-        value={content}
-        onChange={(newValue) => setContent(newValue)}
-        modules={modules}
-        formats={formats}
-      /> */}
+      <Editor onChange={setContent} value={content} />
+
       <button style={{ marginTop: '10px' }}>Создать пост</button>
     </form>
   );
