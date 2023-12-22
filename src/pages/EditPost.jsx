cf3c0402d81022e0407bad8c04af92ca10e25772
Mikhail Saburov <saburov1985@gmail.com>
2023-12-22 23:35:53 +1100

    feature(EditPost)Ж перпенес код с CreatePost

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['link', 'image'],
+    ['clean'],
+  ],
+};
+
+const formats = [
+  'header',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'link',
+  'image',
+];
 
 export const EditPost = () => {
-  return <div>EditPost</div>;
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
+  const [files, setFiles] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  return (
+    <form className="create" onSubmit={EditPost}>
+      <input
+        type="title"
+        placeholder={'Введите заголовок'}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={'Краткое описание'}
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
+      <ReactQuill
+        theme="snow"
+        value={content}
+        onChange={(newValue) => setContent(newValue)}
+        modules={modules}
+        formats={formats}
+      />
+      <button style={{ marginTop: '10px' }}>Создать пост</button>
+    </form>
+  );
 };
