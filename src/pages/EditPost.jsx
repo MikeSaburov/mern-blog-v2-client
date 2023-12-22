7bba736a0e49f3e5c9c70ee4132af380d28cc197
Mikhail Saburov <saburov1985@gmail.com>
2023-12-23 00:07:29 +1100

    feature(EditPost): реализован запрос на изменение поста

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -44,12 +44,27 @@ export const EditPost = () => {
     });
   }, []);
 
-  function updatePost(e) {
+  async function updatePost(e) {
     e.preventDefault();
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    if (files?.[0]) {
+      data.set('file', files?.[0]);
+    }
+    const response = await fetch('http://localhost:4000/post', {
+      method: 'PUT',
+      body: data,
+    });
+    if (response.ok) {
+      alert('Статья успешно добавлена');
+      setRedirect(true);
+    }
   }
 
   if (redirect) {
-    return <Navigate to={'/'} />;
+    return <Navigate to={`/post/${id}`} />;
   }
 
   return (
