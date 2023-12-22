d848786a5951a86e709e5feb08fa848fe6b5b065
Mikhail Saburov <saburov1985@gmail.com>
2023-12-22 23:46:43 +1100

    refactor(Editor, CreatePost): вынес ReactQuill в отдельный компонент

---
+++ src/Editor.js
@@ -0,0 +1,37 @@
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+export default function Editor(value, onChange) {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['link', 'image'],
+      ['clean'],
+    ],
+  };
+
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'link',
+    'image',
+  ];
+
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={onChange}
+      modules={modules}
+      formats={formats}
+    />
+  );
+}
